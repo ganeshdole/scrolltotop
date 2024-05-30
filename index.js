671c319926498898052bfb51ed56f91a481392ca
Ganesh Dole <dole77770@gmail.com>
2024-05-30 12:15:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const ScrollToTop = (props) => {
   const location = useLocation();
 
   useEffect(() => {
-    window.scrollTo(0, 0);
+    window.scrollTo({ top: 0, behavior: "smooth" });
   }, [location]);
 
   return <>{children}</>;
